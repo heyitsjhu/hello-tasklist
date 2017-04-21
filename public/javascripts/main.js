effdf4d941ce1b57962cd996dd5aaa44594544c5
Johnny Hu <jgeehen.hu@gmail.com>
2017-04-20 17:00:01 -0700

    Add comments describing functions

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,11 @@
+// TASKS INDEX PAGE FUNCTIONS
 var taskCompletedIcons = document.getElementsByClassName('task-completed-icon');
 var taskEditIcons = document.getElementsByClassName('task-edit-icon');
 var taskDeleteIcons = document.getElementsByClassName('task-delete-icon');
 var taskItems = document.getElementsByClassName('task-item');
 
+
+// Add/remove Bootstrap 'text-success' class on the check icon.
 for(var i = 0; i < taskCompletedIcons.length; i++) {
   taskCompletedIcons[i].addEventListener('mouseover', function(){
     this.classList.add('text-success');
@@ -17,6 +20,7 @@ for(var i = 0; i < taskCompletedIcons.length; i++) {
   });
 }
 
+// Add/remove Bootstrap 'text-warning' class on the edit icon.
 for(var i = 0; i < taskEditIcons.length; i++) {
   taskEditIcons[i].addEventListener('mouseover', function(){
     this.classList.add('text-warning');
@@ -26,6 +30,7 @@ for(var i = 0; i < taskEditIcons.length; i++) {
   });
 }
 
+// Add/remove Bootstrap 'text-danger' class on the delete icon.
 for(var i = 0; i < taskDeleteIcons.length; i++) {
   taskDeleteIcons[i].addEventListener('mouseover', function(){
     this.classList.add('text-danger');
@@ -40,6 +45,7 @@ for(var i = 0; i < taskDeleteIcons.length; i++) {
   });
 }
 
+// Add/remove class that highlights a task item on the list when hovered over.
 for(var i = 0; i < taskItems.length; i++) {
   taskItems[i].addEventListener('mouseover', function(){
     this.classList.add('task-item-highlight');
@@ -53,21 +59,22 @@ for(var i = 0; i < taskItems.length; i++) {
 var taskCompleteButton = document.getElementById('taskCompleteButton');
 var taskDeleteButton = document.getElementById('taskDeleteButton');
 
-// On click, this submits the parent form——deleting the task.
+// The next two functions submit forms on click.
+// Sets a task's 'completed' property to true.
 if(taskCompleteButton) {
   taskCompleteButton.addEventListener('click', function(){
     this.parentNode.submit();
   });
 }
 
-// On click, this submits the parent form——deleting the task.
+// Removes a task from the database.
 if(taskCompleteButton) {
   taskDeleteButton.addEventListener('click', function(){
     this.parentNode.submit();
   });
 }
 
-// Go back to previous page.
+// Go back to previous page in browser history.
 function goBack() {
   window.history.back();
 }
